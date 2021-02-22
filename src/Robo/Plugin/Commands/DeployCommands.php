f56cd7d5180de0531573cf7196f762f7b0be4930
Mark Dorison <mark@chromatichq.com>
2021-02-22 15:34:35 -0500

    Added Drupal deploy commands.

---
+++ src/Robo/Plugin/Commands/DeployCommands.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CHQRobo\Robo\Plugin\Commands;
+
+use Robo\Exception\TaskException;
+use Robo\Result;
+use Robo\Robo;
+use Robo\Tasks;
+
+/**
+ * Robo commands related to continuous integration.
+ */
+class DeployCommands extends Tasks
+{
+    /**
+     * RoboFile constructor.
+     */
+    public function __construct()
+    {
+        // Treat this command like bash -e and exit as soon as there's a failure.
+        $this->stopOnFail();
+    }
+
+    /**
+     * Run a Drupal 7 deployment.
+     *
+     * @param string $siteName
+     *   The Benz site shortname.
+     * @param string $appDirPath
+     *   The app directory path.
+     *
+     * @return \Robo\Result
+     *   The result of the set of tasks.
+     */
+    public function deployDrupal7(string $appDirPath, string $siteName = 'default'): Result
+    {
+        return $this->taskExecStack()
+        ->dir("web/sites/$siteName/")
+        ->exec('vendor/bin/drush cc all')
+        ->exec('vendor/bin/drush updb --yes')
+        ->exec('vendor/bin/drush fra --yes')
+        ->exec('vendor/bin/drush cc all')
+        ->run();
+    }
+
+    /**
+     * Run a Drupal 8/9 deployment.
+     *
+     * @param string $siteName
+     *   The Benz site shortname.
+     * @param string $appDirPath
+     *   The app directory path.
+     *
+     * @return \Robo\Result
+     *   The result of the set of tasks.
+     */
+    public function deployDrupal(string $appDirPath, string $siteName = 'default'): Result
+    {
+        return $this->taskExecStack()
+        ->dir("$appDirPath/web/sites/$siteName/")
+        ->exec('$appDirPath/vendor/bin/drush deploy -- yes')
+        // Import the latest configuration again. This includes the latest
+        // configuration_split configuration. Importing this twice ensures that
+        // the latter command enables and disables modules based upon the most up
+        // to date configuration. Additional information and discussion can be
+        // found here:
+        // https://github.com/drush-ops/drush/issues/2449#issuecomment-708655673
+        ->exec("$appDirPath/vendor/bin/drush config:import --yes")
+        ->run();
+    }
+}
