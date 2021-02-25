2f7fd768269db332810661edf8bd6716df5d9df2
Mark Dorison <mark@chromatichq.com>
2021-02-25 11:06:50 -0500

    Added theme:build command. (#2)

---
+++ src/Robo/Plugin/Commands/ThemeCommands.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ChqRobo\Robo\Plugin\Commands;
+
+use Robo\Exception\TaskException;
+use Robo\Result;
+use Robo\Robo;
+use Robo\Tasks;
+
+/**
+ * Robo commands related to theme operations.
+ */
+class ThemeCommands extends Tasks
+{
+    /**
+     * RoboFile constructor.
+     */
+    public function __construct()
+    {
+        // Treat this command like bash -e and exit as soon as there's a failure.
+        $this->stopOnFail();
+    }
+
+    /**
+     * Build one or more themes.
+     *
+     * Configure theme build command(s) to be run using the 'theme_build' key
+     * in your robo.yml file.
+     *
+     * @return \Robo\Result
+     *   The result of the set of tasks.
+     */
+    public function themeBuild(): Result
+    {
+        if (empty($themeBuildConfiguration = Robo::config()->get('theme_build'))) {
+            throw new TaskException($this, 'Expected theme configuration not present: theme_build');
+        }
+        $this->io()->title("building themes");
+        foreach ($themeBuildConfiguration as $themeConfig) {
+            $themePath = $themeConfig['theme_path'];
+            $this->io()->section("building theme at $themePath");
+            foreach ($themeConfig['theme_build_commands'] as $themeBuildCommand) {
+                $result = $this->taskExec($themeBuildCommand)
+                    ->dir($themePath)
+                    ->run();
+            }
+        }
+        return $result;
+    }
+}
