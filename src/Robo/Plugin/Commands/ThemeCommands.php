7ac4bbbae84744048bb1ff07356baaecd654e0c2
Mark Dorison <mark@chromatichq.com>
2021-03-28 20:40:57 -0400

    Removed workaround code.

--- src/Robo/Plugin/Commands/ThemeCommands.php
+++ src/Robo/Plugin/Commands/ThemeCommands.php
@@ -35,7 +35,7 @@ class ThemeCommands extends Tasks
      */
     public function themeBuild($siteName = 'default'): Result
     {
-        $themeBuildConfiguration = $this->themeBuildConfiguration($siteName);
+        $themeBuildConfiguration = $this->getConfig('theme_build', $siteName);
         $this->io()->title("theme build");
         foreach ($themeBuildConfiguration as $themeConfig) {
             $themePath = $themeConfig['theme_path'];
@@ -47,38 +47,5 @@ class ThemeCommands extends Tasks
             }
         }
         return $result;
-    }
-
-    /**
-     * Get theme build configuration with fallback.
-     *
-     * Retrieves theme_build configuration for a specified site. If the theme_build config is not
-     * present for the specified site, fall back to theme_build for 'default' if that is present.
-     *
-     * @return array
-     *   The theme_build configuration array.
-     */
-    protected function themeBuildConfiguration($siteName = 'default'): array
-    {
-        $siteConfig = $this->getSiteConfig($siteName);
-        $themeBuildConfiguration = $siteConfig['theme_build'] ?? null;
-        // @todo: Re-evaluate this fallback code. It's gross. All it does is reduce duplication
-        // in the sites config file. Is that worth this complexity?
-        if (empty($themeBuildConfiguration)) {
-            $defautConfig = $this->getSiteConfig('default');
-            $themeBuildConfiguration = $defautConfig['theme_build'] ?? null;
-            if (empty($themeBuildConfiguration)) {
-                throw new TaskException($this, "Expected theme configuration not present for $siteName.");
-            }
-            foreach($themeBuildConfiguration as $key => $steps) {
-                $placeholder = Robo::config()->get('sitename_placeholder');
-                if (strpos($steps['theme_path'], $placeholder) !== false) {
-                    $themeBuildConfiguration[$key]['theme_path'] = str_replace($placeholder, $siteName, $themeBuildConfiguration[$key]['theme_path']);
-                }
-            }
-            $this->say("Custom theme_build configuration not present for $siteName.
-            Falling back to configuration found in 'default'.");
-        }
-        return $themeBuildConfiguration;
     }
 }
