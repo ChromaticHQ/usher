3b03b69b37a24d8148bd837dfb78a26a79649630
Adam Zimmermann <adam@chromatichq.com>
2022-11-17 20:51:54 -0600

    Refactor code.

--- src/Robo/Plugin/Commands/DeployCommands.php
+++ src/Robo/Plugin/Commands/DeployCommands.php
@@ -4,7 +4,6 @@ namespace Usher\Robo\Plugin\Commands;
 
 use Robo\Result;
 use Robo\Tasks;
-use Usher\Robo\Plugin\Traits\ResultCheckTrait;
 use Usher\Robo\Plugin\Traits\SlackNotifierTrait;
 
 /**
@@ -13,8 +12,6 @@ use Usher\Robo\Plugin\Traits\SlackNotifierTrait;
 class DeployCommands extends Tasks
 {
     use SlackNotifierTrait;
-
-    public const TUGBOAT_DASHBOARD_URL = 'https://dashboard.tugboatqa.com';
 
     /**
      * RoboFile constructor.
@@ -84,53 +81,10 @@ class DeployCommands extends Tasks
             // https://github.com/drush-ops/drush/issues/2449#issuecomment-708655673
             ->exec("$appDirPath/vendor/bin/drush config:import --yes")
             ->run();
-        // @todo Do we want/need this option if we can just check for environment variables?
-        // @todo This could be useful if a site wanted to opt out of this functionality.
-        // @todo Perhaps we should rename this option then to be more generic?
+        // Attempt to notify Slack if the "tugboat" option is supplied.
         if ($opts['tugboat']) {
             $this->notifySlackOnFailedBasePreviewBuild($result);
         }
         return $result;
-    }
-
-    /**
-     * Notify Slack if a base preview build failed.
-     *
-     * @param \Robo\Result $result
-     *   The result of the task to check.
-     *
-     * @see https://docs.tugboatqa.com/starter-configs/code-snippets/slack-integration/
-     */
-    protected function notifySlackOnFailedBasePreviewBuild(Result $result): void
-    {
-        // Confirm we are in a Tugboat environment.
-        if (getenv('TUGBOAT_PREVIEW_ID') === false) {
-            $this->say('Skipping Slack notification since no Tugboat preview ID was found.');
-            return;
-        }
-        // Determine if we are building a base preview.
-        // @todo Remove the testing override condition at the end before merging.
-        if (getenv('TUGBOAT_PREVIEW_ID') !== getenv('TUGBOAT_BASE_PREVIEW_ID') && getenv('TUGBOAT_PREVIEW_ID') === '') {
-            $this->say('Skipping Slack notification since this is not a base preview.');
-            return;
-        }
-        // If everything went well there is nothing to do.
-        if ($result->wasSuccessful()) {
-            $this->say('Skipping Slack notification since all tasks completed successfully.');
-            return;
-        }
-        // Build various variables and URLs for the Slack message.
-        $dashboard_url = sprintf(
-            '%s/%s',
-            self::TUGBOAT_DASHBOARD_URL,
-            getenv('TUGBOAT_PREVIEW_ID'),
-        );
-        $text = sprintf(
-            "Tugboat Base Preview failed to build for %s\nPreview: %s\nDashboard: %s",
-            getenv('TUGBOAT_REPO'),
-            getenv('TUGBOAT_SERVICE_URL'),
-            $dashboard_url
-        );
-        $this->notifySlack('Tugboat', $text);
     }
 }
--- src/Robo/Plugin/Traits/SlackNotifierTrait.php
+++ src/Robo/Plugin/Traits/SlackNotifierTrait.php
@@ -3,13 +3,54 @@
 namespace Usher\Robo\Plugin\Traits;
 
 use GuzzleHttp\Client;
-use NotifierInterface;
+use Robo\Result;
 
 /**
  * Trait to provide notification functionality.
  */
 trait SlackNotifierTrait
 {
+    /**
+     * The Tugboat dashboard URL.
+     *
+     * @var string
+     */
+    protected $tugboatDashboardUrl = 'https://dashboard.tugboatqa.com';
+
+    /**
+     * Notify Slack if a base preview build failed.
+     *
+     * @param \Robo\Result $result
+     *   The result of the task to check.
+     *
+     * @see https://docs.tugboatqa.com/starter-configs/code-snippets/slack-integration/
+     */
+    public function notifySlackOnFailedBasePreviewBuild(Result $result): void
+    {
+        // Confirm we are in a base preview.
+        if (!$this->isTugboatBasePreview()) {
+            return;
+        }
+        // If everything went well there is nothing to do.
+        if ($result->wasSuccessful()) {
+            $this->say('Skipping Slack notification since all tasks completed successfully.');
+            return;
+        }
+        // Build various variables and URLs for the Slack message.
+        $dashboard_url = sprintf(
+            '%s/%s',
+            $this->tugboatDashboardUrl,
+            getenv('TUGBOAT_PREVIEW_ID'),
+        );
+        $text = sprintf(
+            "Tugboat Base Preview failed to build for %s\nPreview: %s\nDashboard: %s",
+            getenv('TUGBOAT_REPO'),
+            getenv('TUGBOAT_SERVICE_URL'),
+            $dashboard_url
+        );
+        $this->sendSlackNotification('Tugboat', $text);
+    }
+
     /**
      * Notify Slack.
      *
@@ -20,7 +61,7 @@ trait SlackNotifierTrait
      *
      * @see https://docs.tugboatqa.com/starter-configs/code-snippets/slack-integration/
      */
-    public function notifySlack(string $username, string $text): void
+    protected function sendSlackNotification(string $username, string $text): void
     {
         // Verify that a Slack webhook URL was provided.
         $slack_webhook_url = getenv('SLACK_WEBHOOK_URL');
@@ -35,5 +76,25 @@ trait SlackNotifierTrait
         // Send the Slack webhook call.
         $client = new Client(['timeout' => 5]);
         $client->post($slack_webhook_url, ['body' => json_encode($payload)]);
+    }
+
+    /**
+     * Determine if we are in a Tugboat base preview.
+     *
+     * @return bool
+     */
+    protected function isTugboatBasePreview(): bool
+    {
+        // Confirm we are in a Tugboat environment.
+        if (getenv('TUGBOAT_PREVIEW_ID') === false) {
+            $this->say('No Tugboat preview ID was found.');
+            return false;
+        }
+        // Determine if we are building a base preview.
+        if (getenv('TUGBOAT_PREVIEW_ID') !== getenv('TUGBOAT_BASE_PREVIEW_ID')) {
+            $this->say('No Tugboat base preview found.');
+            return false;
+        }
+        return true;
     }
 }
