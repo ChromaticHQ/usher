e3eb186362385bb4b3c1d49a11c0fea07429e1e6
Adam Zimmermann <adam@chromatichq.com>
2022-12-13 09:34:02 -0600

    Notify Slack when Deployment to a Tugboat Base Preview Fails (#92)
    Co-authored-by: Mark Dorison <mark@chromatichq.com>

--- src/Robo/Plugin/Commands/DeployCommands.php
+++ src/Robo/Plugin/Commands/DeployCommands.php
@@ -2,16 +2,17 @@
 
 namespace Usher\Robo\Plugin\Commands;
 
-use Robo\Exception\TaskException;
 use Robo\Result;
-use Robo\Robo;
 use Robo\Tasks;
+use Usher\Robo\Plugin\Traits\SlackNotifierTrait;
 
 /**
  * Robo commands related to continuous integration.
  */
 class DeployCommands extends Tasks
 {
+    use SlackNotifierTrait;
+
     /**
      * RoboFile constructor.
      */
@@ -54,24 +55,40 @@ class DeployCommands extends Tasks
      *   The Drupal site shortname. Optional.
      * @param string $docroot
      *   The Drupal document root directory. Optional.
+     * @param array $options
+     *   The options.
+     *
+     * @option bool $notify-slack Default to false. If true, Slack
+     *   notification will be sent on build failure in Tugboat.
+     * @option bool $notify-slack-force Default to false. If true, it will force
+     *   an attempt to notify Slack about the build regardless of what happened.
      *
      * @aliases deployd
      *
      * @return \Robo\Result
      *   The result of the set of tasks.
      */
-    public function deployDrupal(string $appDirPath, string $siteName = 'default', string $docroot = 'web'): Result
-    {
-        return $this->taskExecStack()
+    public function deployDrupal(
+        string $appDirPath,
+        string $siteName = 'default',
+        string $docroot = 'web',
+        array $options = ['notify-slack' => false, 'notify-slack-force' => false]
+    ): Result {
+        $result = $this->taskExecStack()
             ->dir("$appDirPath/$docroot/sites/$siteName")
             ->exec("$appDirPath/vendor/bin/drush deploy --yes")
             // Import the latest configuration again. This includes the latest
-            // configuration_split configuration. Importing this twice ensures that
-            // the latter command enables and disables modules based upon the most up
-            // to date configuration. Additional information and discussion can be
-            // found here:
+            // configuration_split configuration. Importing this twice ensures
+            // that the latter command enables and disables modules based upon
+            // the most up-to-date configuration. Additional information and
+            // discussion can be found here:
             // https://github.com/drush-ops/drush/issues/2449#issuecomment-708655673
             ->exec("$appDirPath/vendor/bin/drush config:import --yes")
             ->run();
+        // Notify Slack about the failed deployment if the option was provided.
+        if ($options['notify-slack'] || $options['notify-slack-force']) {
+            $this->notifySlackOnFailedBasePreviewBuild($result, $options['notify-slack-force']);
+        }
+        return $result;
     }
 }
---
+++ src/Robo/Plugin/Traits/SlackNotifierTrait.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Usher\Robo\Plugin\Traits;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Robo\Result;
+
+/**
+ * Trait to provide notification functionality.
+ */
+trait SlackNotifierTrait
+{
+    /**
+     * The Tugboat dashboard URL.
+     *
+     * @var string
+     */
+    protected $tugboatDashboardUrl = 'https://dashboard.tugboatqa.com';
+
+    /**
+     * Notify Slack if a base preview build failed.
+     *
+     * @param \Robo\Result $result
+     *   The result of the task to check.
+     * @param bool $force
+     *   If the notification should be forced.
+     *
+     * @see https://docs.tugboatqa.com/starter-configs/code-snippets/slack-integration/
+     */
+    protected function notifySlackOnFailedBasePreviewBuild(Result $result, bool $force = false): void
+    {
+        // Confirm we are in a base preview.
+        if (!$this->isTugboatBasePreview() && !$force) {
+            return;
+        }
+        // If everything went well there is nothing to do.
+        if ($result->wasSuccessful() && !$force) {
+            $this->say('Skipping Slack notification since all tasks completed successfully.');
+            return;
+        }
+        // Build various variables and URLs for the Slack message.
+        $dashboard_url = sprintf(
+            '%s/%s',
+            $this->tugboatDashboardUrl,
+            getenv('TUGBOAT_PREVIEW_ID'),
+        );
+        $text = sprintf(
+            "Tugboat <%s|Base Preview> failed to build for *%s*.",
+            $dashboard_url,
+            getenv('TUGBOAT_REPO')
+        );
+        $this->sendSlackNotification('Tugboat', $text);
+    }
+
+    /**
+     * Notify Slack.
+     *
+     * @param string $username
+     *   The username to pass to Slack.
+     * @param string $text
+     *   The text string to pass to the Slack API.
+     *
+     * @see https://docs.tugboatqa.com/starter-configs/code-snippets/slack-integration/
+     */
+    protected function sendSlackNotification(string $username, string $text): void
+    {
+        // Verify that a Slack webhook URL was provided.
+        $slack_webhook_url = getenv('SLACK_WEBHOOK_URL');
+        if ($slack_webhook_url === false || $slack_webhook_url === '') {
+            $this->yell('Missing Slack Webhook URL from the "SLACK_WEBHOOK_URL" environment variable.');
+            return;
+        }
+        $payload = [
+            'username' => $username,
+            'text' => $text,
+        ];
+        // Send the Slack webhook call.
+        try {
+            $client = new Client(['timeout' => 5]);
+            $client->post($slack_webhook_url, ['body' => json_encode($payload)]);
+        } catch (RequestException $exception) {
+            $this->yell('Slack webhook request failed.');
+        }
+    }
+
+    /**
+     * Determine if we are in a Tugboat base preview.
+     *
+     * @return bool
+     *   Boolean indicating if we are interacting with a Tugboat base preview.
+     */
+    protected function isTugboatBasePreview(): bool
+    {
+        // Confirm we are in a Tugboat environment.
+        if (getenv('TUGBOAT_PREVIEW_ID') === false) {
+            return false;
+        }
+        // Determine if we are building a base preview.
+        if (getenv('TUGBOAT_PREVIEW_ID') !== getenv('TUGBOAT_BASE_PREVIEW_ID')) {
+            $this->say('No Tugboat base preview found.');
+            return false;
+        }
+        return true;
+    }
+}
