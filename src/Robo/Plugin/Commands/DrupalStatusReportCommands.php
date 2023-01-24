1c25a5e35d3a5f627a5bffb3fc96833119eaba16
Mark Dorison <mark@chromatichq.com>
2023-01-23 19:24:45 -0500

    Added Drupal status report command.

---
+++ src/Robo/Plugin/Commands/DrupalStatusReportCommands.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Usher\Robo\Plugin\Commands;
+
+use DrupalFinder\DrupalFinder;
+use Robo\Exception\TaskException;
+use Robo\Robo;
+use Robo\Tasks;
+
+/**
+ * Robo commands to work with Drupal's status report.
+ */
+class DrupalStatusReportCommands extends Tasks
+{
+    /**
+     * Drupal root directory.
+     *
+     * @var string
+     */
+    protected $drupalRoot;
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        // Treat this command like bash -e and exit as soon as there's a failure.
+        $this->stopOnFail();
+
+        // Find Drupal root path.
+        $drupalFinder = new DrupalFinder();
+        $drupalFinder->locateRoot(getcwd());
+        $this->drupalRoot = $drupalFinder->getDrupalRoot();
+    }
+
+    /**
+     * Validate Drupal status report.
+     *
+     * @param string $siteDir
+     *   The Drupal site directory.
+     * @param int $severity
+     *   The minimum severity level to show. Defaults to 1 (warning).
+     *
+     * @throws \Robo\Exception\TaskException
+     */
+    public function drupalStatusReport($siteDir = 'default', $severity = 1): void
+    {
+        $ignoreList = implode(',', Robo::config()->get('drupal_status_report_ignore_checks'));
+        $result = $this->taskExec("$this->drupalRoot/../vendor/bin/drush status-report --format=json
+            --severity=$severity --ignore=$ignoreList")
+            ->dir("$this->drupalRoot/sites/$siteDir/")
+            ->printOutput(false)
+            ->run();
+        $drushOutput = trim($result->getOutputData());
+        $reportJson = json_decode($drushOutput);
+        if (!is_array($reportJson) || count($reportJson) > 0) {
+            $this->say($drushOutput);
+            throw new TaskException(
+                $this,
+                'Drupal status report shows one or more unexpected warnings or errors!'
+            );
+        }
+        $this->say('Drupal status report shows no unexpected warnings or errors.');
+    }
+}
