15efdbc709b4636daeeacad8a5a30345a894ea2c
Mark Dorison <mark@chromatichq.com>
2023-04-20 18:47:49 -0400

    Added check to determine if we are running on D7.

--- src/Robo/Plugin/Traits/DrupalVersionTrait.php
+++ src/Robo/Plugin/Traits/DrupalVersionTrait.php
@@ -3,13 +3,27 @@
 namespace Usher\Robo\Plugin\Traits;
 
 /**
- * Trait to determine what version of Drupal is being run..
+ * Trait to determine what version of Drupal is being run.
  */
 trait DrupalVersionTrait
 {
+    /**
+     * Check if version of Drupal in question is Drupal 7.
+     *
+     * @param string $drupalRootPath
+     *   The path to the Drupal root.
+     */
     protected function drupalVersionIsD7(string $drupalRootPath): bool
     {
-        // @TODO: Implement check.
+        // This line has not changed since 2013, so it seems safe to base our version check off of.
+        // https://git.drupalcode.org/project/drupal/-/blame/7.x/COPYRIGHT.txt
+        $copyrightString = 'All Drupal code is Copyright 2001 - 2013 by the original authors.';
+        $copyrightPath = "$drupalRootPath/COPYRIGHT.txt";
+        if (file_exists($copyrightPath) && $copyrightContents = file_get_contents($copyrightPath)) {
+            if (str_contains($copyrightContents, $copyrightString)) {
+                return true;
+            }
+        }
         return false;
     }
 }
