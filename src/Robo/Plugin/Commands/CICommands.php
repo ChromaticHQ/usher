b96424de8d270f8ffe684de30bb2ff6d672ea114
Mark Dorison <mark@chromatichq.com>
2023-06-29 12:11:05 -0400

    backport: Update phpcsversion to use php_current_version.
    https://github.com/ChromaticHQ/usher/commit/7f85b79198b72ad2178534f2c9eb241e4f1cdf61

--- src/Robo/Plugin/Commands/CICommands.php
+++ src/Robo/Plugin/Commands/CICommands.php
@@ -104,7 +104,7 @@ class CICommands extends Tasks
         $phpcsStandards = implode(',', $this->getRequiredRoboConfigArrayFor('phpcs_standards'));
         $phpcsCheckExtensions = implode(',', $this->getRequiredRoboConfigArrayFor('phpcs_check_extensions'));
         $phpcsIgnorePaths = implode(',', $this->getRequiredRoboConfigArrayFor('phpcs_ignore_paths'));
-        $phpcsPhpVersion = Robo::config()->get('phpcs_php_version', $this::PHPCS_DEFAULT_PHP_VERSION);
+        $phpcsPhpVersion = Robo::config()->get('php_current_version', $this::PHPCS_DEFAULT_PHP_VERSION);
         $twigLintEnabled = Robo::config()->get('twig_lint_enable') ?? true;
 
         /** @var \Robo\Task\CommandStack $stack */
