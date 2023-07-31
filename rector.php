e37ff319e3be33d89daa592438c5ce013ddac4ed
Mark Dorison <mark@chromatichq.com>
2023-07-31 15:42:47 -0400

    Disabled MixedTypeRector.
    I don't mind removing argument docblocks generally, but in this case
    robo uses them to populate help text so I think we are actually losing
    some valuable help info.

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use Rector\Php71\Rector\FuncCall\CountOnNullRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -20,6 +21,7 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->skip([
+        MixedTypeRector::class,
         CountOnNullRector::class => [
             // @see https://github.com/rectorphp/rector/issues/8016
             __DIR__ . '/src/Robo/Plugin/Traits/SitesConfigTrait.php',
